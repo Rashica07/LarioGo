b760a4b3d19f2c422ad360153a6874487d961e53
KodibKFG <bibfrrokaj@yahoo.com>
2026-06-29 00:09:12 +0200

    Initial Commit

---
+++ LarioGo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LarioGo
+//
+//  Created by user on 29.6.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
